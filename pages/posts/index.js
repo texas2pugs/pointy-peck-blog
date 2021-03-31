0879eefa45599723d42fd7a8fd74e3990e23ce1c
Christopher Inman <christophereinman@gmail.com>
2021-03-31 13:02:22 -0500

    Added All Posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,31 @@
-function AllPostsPage() {}
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'new-establishment',
+    title: 'New Establishment!',
+    excerpt: 'The Pecking Pointe: Established 2021',
+    date: '2021-03-31',
+    image: 'the-pecking-pointe.jpg',
+  },
+  {
+    slug: 'new-establishment2',
+    title: 'New Establishment!',
+    excerpt: 'The Pecking Pointe: Established 2021',
+    date: '2021-03-31',
+    image: 'the-pecking-pointe.jpg',
+  },
+  {
+    slug: 'new-establishment3',
+    title: 'New Establishment!',
+    excerpt: 'The Pecking Pointe: Established 2021',
+    date: '2021-03-31',
+    image: 'the-pecking-pointe.jpg',
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
