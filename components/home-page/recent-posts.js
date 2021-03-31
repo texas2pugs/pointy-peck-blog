b754f329982013a811bb17c39a8b4da526f96e90
Christopher Inman <christophereinman@gmail.com>
2021-03-31 12:04:29 -0500

    Rendering dummy data

--- components/home-page/recent-posts.js
+++ components/home-page/recent-posts.js
@@ -1,9 +1,12 @@
+import PostsGrid from '../posts/posts-grid.js';
+
 import classes from './recent-posts.module.css';
 
-function RecentPosts() {
+function RecentPosts(props) {
   return (
     <section className={classes.latest}>
       <h2>Recent Posts</h2>
+      <PostsGrid posts={props.posts} />
     </section>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
 import { Fragment } from 'react';
 
 import Venue from '../components/home-page/venue.js';
+import RecentPosts from '../components/home-page/recent-posts.js';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'new-establishment',
+    title: 'New Establishment!',
+    excerpt: 'The Pecking Pointe: Established 2021',
+    date: '2021-03-31',
+    image: 'the-pecking-pointe.jpg',
+  },
+  {
+    slug: 'new-establishment2',
+    title: 'New Establishment!',
+    excerpt: 'The Pecking Pointe: Established 2021',
+    date: '2021-03-31',
+    image: 'the-pecking-pointe.jpg',
+  },
+  {
+    slug: 'new-establishment3',
+    title: 'New Establishment!',
+    excerpt: 'The Pecking Pointe: Established 2021',
+    date: '2021-03-31',
+    image: 'the-pecking-pointe.jpg',
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Venue />
+      <RecentPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
