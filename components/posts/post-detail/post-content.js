e291b644edf17c9db7ae94fcbf4f2aef5f7525b6
Christopher Inman <christophereinman@gmail.com>
2021-04-01 07:39:55 -0500

    Added ReactMarkdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,25 +1,25 @@
-import PostHeader from './post-header';
+import ReactMarkdown from 'react-markdown';
 
+import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'new-establishment',
-    title: 'New Establishment!',
-    content: '# This is a first post',
-    date: '2021-03-31',
-    image: 'the-pecking-pointe.jpg',
-  },
-];
+const DUMMY_POSTS = {
+  slug: 'new-establishment',
+  title: 'New Establishment!',
+  content: '# This is a first post',
+  date: '2021-03-31',
+  image: 'the-pecking-pointe.jpg',
+};
 
 function PostContent() {
   console.log('Inside PostContent()');
+  console.log(DUMMY_POSTS);
   const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`;
 
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POSTS.title} image={imagePath} />
-      {DUMMY_POSTS.content}
+      <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
     </article>
   );
 }
