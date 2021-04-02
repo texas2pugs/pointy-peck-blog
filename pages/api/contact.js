847bd98a59472762a522d948b8669ebff72037e2
Christopher Inman <christophereinman@gmail.com>
2021-04-02 08:34:52 -0500

    Added contact api route

---
+++ pages/api/contact.js
@@ -0,0 +1,30 @@
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !message ||
+      message.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input.' });
+      return;
+    }
+
+    // Store in a database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    console.log(newMessage);
+
+    res.status(201).json({ message: 'Message received' });
+  }
+}
+
+export default handler;
